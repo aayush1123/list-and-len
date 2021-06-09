ea5f0936ece190056bae7ea8f18c6f01c852faad
aayush pradhan <78127930+aayush1123@users.noreply.github.com>
2021-06-09 08:42:47 +0545

    Add files via upload

---
+++ listnlen.py
@@ -0,0 +1,10 @@
+fruits=["apple","orange","graphs","banana..."]#this is list
+print(len(fruits))#count the numbers of string or numbers
+print(len("aayush "))
+print(fruits[0:2])
+print(fruits[0]+fruits[3])
+print(fruits)
+num=[1,2,3,4,"hi"]#both string and numbers can be include in the same list
+num.append("hello")#append is used to add something on the list at the end 
+print(num)
+print(num[3])
